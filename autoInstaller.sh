16376a20beb2768a7fc171cc6be63ce87b658ba4
Faiq98 <fh.faiq@gmail.com>
2020-01-20 23:45:11 +0800

    compilation

---
+++ autoInstaller.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget https://raw.githubusercontent.com/Faiq98/JavaHosting/master/TomcatInstaller.sh
+wget https://raw.githubusercontent.com/Faiq98/JavaHosting/master/MySQLInstaller.sh
+wget https://raw.githubusercontent.com/Faiq98/JavaHosting/master/phpMyAdmin.sh
