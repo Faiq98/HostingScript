a6dd37dcef951c3d7e970cfa3db677d035d24905
Faiq98 <fh.faiq@gmail.com>
2020-01-20 23:03:06 +0800

    update MySQLInstaller.sh

--- MySQLInstaller.sh
+++ MySQLInstaller.sh
@@ -10,10 +10,11 @@ sudo apt install mysql-server
 sudo mysql_secure_installation
 
 #Adjusting User Auth & Privilagr
-sudo mysql
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
-FLUSH PRIVILEGES;
-exit
+
+CMD1="ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';"
+CMD2="FLUSH PRIVILEGES;"
+CMD3="exit"
+sudo mysql -Bse "$CMD1;$CMD2;$CMD3"
 
 #Check MySQL status
 systemctl status mysql.service
