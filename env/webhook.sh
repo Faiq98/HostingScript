a0fb7cf3fbb8f742c6c8f9286f78870dd4d2c322
Faiq98 <fh.faiq@gmail.com>
2020-10-29 22:22:42 +0800

    skip commit message

--- env/webhook.sh
+++ env/webhook.sh
@@ -21,7 +21,7 @@ http.createServer(function (req, res) {
         let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
 
         if (req.headers['x-hub-signature'] == sig) {
-            exec('cd ' + repo + ' && git pull');
+            exec('cd ' + repo + ' && git pull --no-edit');
         }
     });
 
