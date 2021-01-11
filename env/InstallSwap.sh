1cf870a231d7dfd8367df97de2d7ed534389ba99
Faiq98 <fh.faiq@gmail.com>
2021-01-11 11:13:35 +0800

    swap file for gcp

---
+++ env/InstallSwap.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo apt update && sudo apt upgrade -y
+
+sudo fallocate -l 1G /swapfile  
+sudo dd if=/dev/zero of=/swapfile bs=1024 count=1048576  
+sudo chmod 600 /swapfile  
+sudo mkswap /swapfile  
+sudo swapon /swapfile
+
+echo /swapfile swap swap defaults 0 0 >> /etc/fstab
